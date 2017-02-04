19bf53fc9e5157465cab5c05c99a774d34a0ecdd
pratikg17 <pratikg17.wohlig@gmail.com>
2017-02-04 10:44:04 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,118 @@
+// JavaScript Document
+var firstapp = angular.module('firstapp', [
+    'ui.router',
+    'phonecatControllers',
+    'templateservicemod',
+    'navigationservice',
+    'pascalprecht.translate',
+    'angulartics',
+    'angulartics.google.analytics'
+]);
+
+firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
+    // for http request with session
+    $httpProvider.defaults.withCredentials = true;
+    $stateProvider
+        .state('home', {
+            url: "/",
+            templateUrl: "views/template.html",
+            controller: 'HomeCtrl'
+        })
+         .state('about', {
+            url: "/about",
+            templateUrl: "views/template.html",
+            controller: 'AboutCtrl'
+        })
+        .state('calculator', {
+            url: "/calculator",
+            templateUrl: "views/template.html",
+            controller: 'CalculatorCtrl'
+        })
+        .state('about_us', {
+            url: "/about_us",
+            templateUrl: "views/template.html",
+            controller: 'About_usCtrl'
+        })
+         .state('blog-individual', {
+            url: "/blog-individual",
+            templateUrl: "views/template.html",
+            controller: 'Blog-IndividualCtrl'
+        })
+          .state('blog', {
+            url: "/blog",
+            templateUrl: "views/template.html",
+            controller: 'BlogCtrl'
+        })
+        .state('form', {
+            url: "/form",
+            templateUrl: "views/template.html",
+            controller: 'FormCtrl'
+        });
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(isproduction);
+});
+
+
+firstapp.directive('img', function($compile, $parse) {
+    return {
+        restrict: 'E',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            if (!attrs.noloading) {
+                $element.after("<img src='img/loading.gif' class='loading' />");
+                var $loading = $element.next(".loading");
+                $element.load(function() {
+                    $loading.remove();
+                    $(this).addClass("doneLoading");
+                });
+            } else {
+                $($element).addClass("doneLoading");
+            }
+        }
+    };
+});
+
+firstapp.directive('fancybox', function($document) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function(scope, element, attr) {
+            var $element = $(element);
+            var target;
+            if (attr.rel) {
+                target = $("[rel='" + attr.rel + "']");
+            } else {
+                target = element;
+            }
+
+            target.fancybox({
+                openEffect: 'fade',
+                closeEffect: 'fade',
+                closeBtn: true,
+                padding: 0,
+                helpers: {
+                    media: {}
+                }
+            });
+        }
+    };
+});
+
+firstapp.directive('autoHeight', function($compile, $parse) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            var windowHeight = $(window).height();
+            $element.css("min-height", windowHeight);
+        }
+    };
+});
+
+firstapp.config(function($translateProvider) {
+    $translateProvider.translations('en', LanguageEnglish);
+    $translateProvider.translations('hi', LanguageHindi);
+    $translateProvider.preferredLanguage('en');
+});
---
+++ js/controllers.js
@@ -0,0 +1,130 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ksSwiper'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    // $scope.mySlides = [
+    //     'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    //     'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    //     'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    //     'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+    // ];
+     $scope.section = {
+     one: "views/content/section/section1.html",
+     two: "views/content/section/section2.html"
+   };
+})
+
+.controller('FormCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('AboutCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("about"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("About"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('About_usCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("about_us"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("About_us"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('CalculatorCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("calculator"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Calculator"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('Blog-IndividualCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("blog-individual"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Blog-Individual"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('BlogCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+    $scope.template = TemplateService.changecontent("blog"); //Use same name of .html file
+    $scope.menutitle = NavigationService.makeactive("Blog"); //This is the Title of the Website
+    TemplateService.title = $scope.menutitle;
+    $scope.navigation = NavigationService.getnav();
+
+    $scope.formSubmitted = false;
+
+    $scope.submitForm = function(data) {
+        console.log(data);
+        $scope.formSubmitted = true;
+    }
+})
+.controller('headerctrl', function($scope, TemplateService) {
+    $scope.template = TemplateService;
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+        $(window).scrollTop(0);
+    });
+    $.fancybox.close(true);
+})
+
+.controller('languageCtrl', function($scope, TemplateService, $translate, $rootScope) {
+
+    $scope.changeLanguage = function() {
+        console.log("Language CLicked");
+
+        if (!$.jStorage.get("language")) {
+            $translate.use("hi");
+            $.jStorage.set("language", "hi");
+        } else {
+            if ($.jStorage.get("language") == "en") {
+                $translate.use("hi");
+                $.jStorage.set("language", "hi");
+            } else {
+                $translate.use("en");
+                $.jStorage.set("language", "en");
+            }
+        }
+        //  $rootScope.$apply();
+    };
+
+
+})
+
+;
